cbcee62474a19f089affcac5fc0af82b8b2bb6c1
reebaqureshi <reebaqureshi0@icloud.com>
2024-08-28 15:20:38 +0500

    Update indexx.js

--- indexx.js
+++ indexx.js
@@ -7,6 +7,17 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/malaabis', (req, res) => {
+    res.send("<h1>VISIT INSTAGRAM FOR NEW CLOTHS<h1>")
+  })
+  app.get('/youtube', (req, res) => {
+    res.send("<h2>het there<h2>")
+
+  })
+  app.get('/login', (req, res) => {
+    res.send("<h2>enter your password<h2>")
+    
+  })
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
